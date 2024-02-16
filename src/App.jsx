42c48b1d95d9c6c54c88aadc98f73150de5fefce
Judi-legend <judilegend2@gmail.com>
2024-02-17 01:13:14 +0300

    adding new animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import './App.css'
 import React from "react";
 import { motion } from "framer-motion";
 import { ThreeDCardDemo } from './components/ThreeDCardDemo';
+import { SVGMaskEffectDemo } from './components/svgMaskDemo';
+import { HeroParallaxDemo } from './components/HeroParallaxDemo';
 function App() {
 
   return (
     <>
     <ThreeDCardDemo/>
+    <SVGMaskEffectDemo/>
+    <HeroParallaxDemo/>
     </>
   )
 }
