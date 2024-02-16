bd4649f3f17485fe6667f688861a1f690a8da629
Judi-legend <judilegend2@gmail.com>
2024-02-16 23:54:38 +0300

    my animation Aceternity UI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import React from "react";
+import { motion } from "framer-motion";
+import { ThreeDCardDemo } from './components/ThreeDCardDemo';
+function App() {
+
+  return (
+    <>
+    <ThreeDCardDemo/>
+    </>
+  )
+}
+
+export default App
